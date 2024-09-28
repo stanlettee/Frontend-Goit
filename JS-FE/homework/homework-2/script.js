009404cfc054ec42cea28549178721c1f7205663
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2024-09-28 15:29:18 +0100

    homework 2

--- JS-FE/homework/homework-2/script.js
+++ JS-FE/homework/homework-2/script.js
@@ -1,9 +1,43 @@
 let celsius = 45;
 let fahrenheit = (celsius * 9/5) + 32;
-console.log('Temperature in fahrenheit will be:', fahrenheit+'F')
+console.log(`Temperature in fahrenheit will be: ${fahrenheit}F`)
 
 let daysInMonth = 31;
 let hoursInMonth = daysInMonth * 24;
 let minutesInMonth = hoursInMonth * 60;
-console.log('Number of hours in a month: ', hoursInMonth);
-console.log('Numbe rof minutes in a month: ', minutesInMonth)
+console.log(`Number of hours in a month: ${hoursInMonth}`);
+console.log(`Number of minutes in a month: ${minutesInMonth}`)
+
+let playerHealth = 50;
+let playerEnergy = 50;
+playerHealth =- 40;
+playerEnergy =- 30;
+console.log(`Level of player health: ${playerHealth}`);
+console.log(`Level of player energy: ${playerEnergy}`);
+
+let grocerySum = 500;
+let grocerySale = 0.10;
+let finalSum = grocerySum * grocerySale;
+console.log(`Sum of your purchase before sale: ${grocerySum}`)
+console.log(`Sum of sale: ${grocerySale}`)
+console.log(`Sum of your purchase after sale: ${finalSum}`)
+
+let floatNumber = 8.39;
+let floorNumber = Math.floor(floatNumber);
+console.log(`Number for flooring: ${floatNumber}`);
+console.log(`Nubmer which is floored: ${floorNumber}`);
+
+let stringFloat = '3.1415926535';
+let convertedFloat = parseInt(stringFloat)
+console.log(`Converted nubmer: ${convertedFloat}`)
+
+let number = 26;
+sqrtNumber = Math.sqrt(number);
+console.log(`Your number in square root: ${sqrtNumber}`)
+
+let intNumber = 53;
+let stringNumber = '53';
+let parsedInt = parseInt(stringNumber);
+let convertedString = integerValue.toString();
+console.log(`Converted string into integer: ${parsedInt}`);
+console.log(`Converted integer into string: ${convertedString}`);
