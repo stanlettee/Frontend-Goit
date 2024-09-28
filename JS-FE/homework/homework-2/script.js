b418457d5dd7e840c09364737e50267be7f7fcc9
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2024-09-28 15:05:17 +0100

    homework 2

---
+++ JS-FE/homework/homework-2/script.js
@@ -0,0 +1,9 @@
+let celsius = 45;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log('Temperature in fahrenheit will be:', fahrenheit+'F')
+
+let daysInMonth = 31;
+let hoursInMonth = daysInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+console.log('Number of hours in a month: ', hoursInMonth);
+console.log('Numbe rof minutes in a month: ', minutesInMonth)
