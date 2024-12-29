436c8e046e403abea94596844547a2afca6e2d45
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2024-12-29 17:35:09 +0000

    practice

--- JS-FE/practice-js/errays/arrayMethods.js
+++ JS-FE/practice-js/errays/arrayMethods.js
@@ -1,13 +1,90 @@
 // метод forЕach переберає масив та робить щось з кожним елеменом масиву, нічого не повертає
 
-const friends = ['John', 'Jack', 'Joe'];
-friends.forEach((friend, index) => {
-    console.log(`Друг ${friend}, має індекс ${index}`)
-})
+// const friends = ['John', 'Jack', 'Joe'];
+// friends.forEach((friend, index) => {
+//     console.log(`Друг ${friend}, має індекс ${index}`)
+// })
 
 // синтаксис перебераючих методів 
 // назвамасива.назваМетоду((елементМасиву, інлексЕлементу) => {тут расписуємо що будемо робити з елементом масиву})
 
 // Метод map переберає масив та з кожним елементом масиву робить те що ми вказали в тілі колбеку, і цей змінений елемент записує у новий масив
-const updateFriends = friends.map((friend) => `${friend} is my friend`)
-console.log(updateFriends)
+// const updateFriends = friends.map((friend) => `${friend} is my friend`)
+// console.log(updateFriends)
+
+const players = [
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+  ];
+
+//   Використовуючи 👆 масив об’єктів виконай наступні завдання
+//   1. Отримати масив імен всіх гравців
+//   2. Збільшити кількість поінтів кожного гравця на 10% (розпорошуємо старий об'єкт)
+//   3. Збільшити кількість годин гравця по id. Переписати на тернарник
+//   4. Отримати масив всіх гравців онлайн
+//   5. Отримати масив всіх гравців офлайн
+//   6. Oтримати масив всіх хардкорних гравців з часом більше 250
+//   Знайти гравця по id
+//   Знайти гравця по імені
+//   Перевірити чи всі гравці мають час більше 200
+//   Перевірити чи всі гравці онлайн
+
+const allPlayers = players.map((player) => player.name)
+console.log(allPlayers)
+
+// const addPoints = players.map((player) => {
+//     const addedPoints = ((player.points / 100) * 10) + player.points
+//     const updatePlayer = {...player, points: addedPoints} 
+//     // Розшифруємо 40 рядок:
+//     // const updatePlayer = { 
+//     //     id: 'player-1',
+//     //     name: 'Mango',
+//     //     timePlayed: 310, 
+//     //     points: 59.4,
+//     //     online: false,
+//     //     points: addedPoints
+//     // }
+//     return updatePlayer
+// })
+// console.log(addPoints)
+// console.log(players)
+
+const addPoints = players.map((player) => ({...player, points: ((player.points / 100) * 10) + player.points}))
+console.log(addPoints)
+console.log(players)
+
+const playerToFind = players.find((player) => player.id === 'player-3')
+console.log(playerToFind)
+function addHours(hours, player){
+    return player.timePlayed + hours
+}
+console.log(addHours(50, playerToFind))
+
+console.log(players.filter((player) => player.online))
+console.log(players.filter((player) => player.online === false))
+console.log(players.filter((player) => player.timePlayed > 250))
+const playerToFind1 = players.find((player) => player.id === 'player-5')
+const playerToFind2 = players.find((player) => player.name === 'Mango')
+// console.log(players.map((player) => player.timePlayed > 200))
+// console.log(players.map((player) => player.online))
+
+// метод every перебирає масив та перевіряє чи кожне значення з цього масива відповідає заданій умові; якщо всі елементи пройшли перевірку то метод every поверне true
+const array = [15, 13, 5, 23, 45]
+const isNumberEven = array.every((num) => num % 2 !== 0)
+console.log(isNumberEven)
+
+// метод some перебирає масив та перевіряє чи хоча б одне значення масиву відповідає заданій умові
+const numbers = [90, 4, 150, 23]
+const hasBigNumber = numbers.some((number) => number > 100)
+console.log(hasBigNumber)
+
+const checkHours = [310, 470, 230, 150, 80]
+const isNumber = checkHours.every((player) => player.timePlayed > 250)
+console.log(isNumber)
+
+const checkOnline = [false, true, true, false, true]
+const online = checkOnline.every((player) => player.online)
+console.log(online)
--- JS-FE/practice-js/objects/objectsMethods.js
+++ JS-FE/practice-js/objects/objectsMethods.js
@@ -88,15 +88,70 @@
 // const {userName, isStudent, friends, adress: {country, city, street}} = user;
 // console.log(userName, isStudent, friends, country, city, street)
 
-const password = '34534673.Q';
-const newUser = {
-    login: 'hello',
-    password: '31415926user',
+// const password = '34534673.Q';
+// const newUser = {
+//     login: 'hello',
+//     password: '31415926user',
+// }
+// const {login, password: userPassword, avatar = 'avatar11'} = newUser; // avatar = 'avatar11' це ми задали дефолтне значення для ключа який може не прийти з об'єкта
+// console.log(password)
+
+const musicLibrary = {
+    count: 2,
+    artists: [
+      {
+        name: 'The Beatles',
+        albums: [
+          {
+            title: 'Sgt. Pepper\'s Lonely Hearts Club Band',
+            year: 1967,
+            tracks: [
+              { title: 'With a Little Help from My Friends', duration: '2:44' },
+              { title: 'Lucy in the Sky with Diamonds', duration: '3:28' },
+              { title: 'A Day in the Life', duration: '5:33' }
+            ]
+          },
+          {
+            title: 'Abbey Road',
+            year: 1969,
+            tracks: [
+              { title: 'Come Together', duration: '4:19' },
+              { title: 'Something', duration: '3:01' },
+              { title: 'Here Comes the Sun', duration: '3:06' }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Pink Floyd',
+        albums: [
+          {
+            title: 'The Wall',
+            year: 1979,
+            tracks: [
+              { title: 'Another Brick in the Wall, Part 2', duration: '3:59' },
+              { title: 'Comfortably Numb', duration: '6:23' },
+              { title: 'Hey You', duration: '4:40' }
+            ]
+          },
+          {
+            title: 'Dark Side of the Moon',
+            year: 1973,
+            tracks: [
+              { title: 'Speak to Me/Breathe', duration: '3:58' },
+              { title: 'Time', duration: '7:06' },
+              { title: 'Money', duration: '6:22' }
+            ]
+          }
+        ]
+      }        
+    ]
 }
-const {login, password: userPassword, avatar = 'avatar11'} = newUser; // avatar = 'avatar11' це ми задали дефолтне значення для ключа який може не прийти з об'єкта
-console.log(password)
 
-
+const { count, artists } = musicLibrary;
+const [beatles, pinkFloyd] = artists;
+const {beatlesName, beatlesAlbums} = beatles;
+const [stg, abbey] = beatlesAlbums;
 
 
 
