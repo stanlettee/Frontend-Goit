c65d09c67c8acb9289a032e7d2801006dcbe2968
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-01-05 17:34:04 +0000

    practice oop

---
+++ JS-FE/practice-js/oop/script.js
@@ -0,0 +1,104 @@
+const car = {
+    wheels: 4,
+    healights: 2,
+    engine: 'Diesel engine',
+    title: 'BMW'
+}
+const bmwX3 = Object.create(car)
+bmwX3.color = 'blue';
+bmwX3.model = 'X3';
+
+// для того щоб перевикористовувати модель для екземплярів придумали функції конструктори
+
+// const Car = function(wheelsCar, headlightsCar, modelCar, priceCar){
+//     this.wheels = wheelsCar;
+//     this.headlights = headlightsCar;
+//     this.model = modelCar;
+//     this.price = priceCar
+// }
+// const car1 = new Car(4, 2, 'BMW i8', 100000)
+// console.log(car1)
+// const car2 = new Car(4, 2, 'BMW M4')
+
+// const Car = function({wheelsCar, headlightsCar, modelCar, priceCar}){
+//     this.wheels = wheelsCar;
+//     this.headlights = headlightsCar;
+//     this.model = modelCar;
+//     this.price = priceCar
+// }
+// const data1 = {
+//     wheelsCar: 4,
+//     headlightsCar: 2,
+//     modelCar: 'BMW X7',
+//     priceCar: 160000
+// }
+// const data2 = {
+//     wheelsCar: 4,
+//     headlightsCar: 2,
+//     modelCar: 'BMW M4',
+//     priceCar: 150000
+// }
+// const car1 = new Car(data1)
+// console.log(car1)
+// const car2 = new Car(data2)
+// console.log(car2)
+
+// Перепишемо цю функцію на класс: 
+
+// class Car {
+//     constructor ({wheels, headlights, model, price}){
+//         // Це функція в який ми зберігаємо данні. Якщо ми ії не створили то вона автоматично створется і буде порожньою
+//         this.wheels = wheels;
+//         this.headlights = headlights;
+//         this.model = model;
+//         this.price = price
+//     }
+//     showInfo(){
+//         return `Автомобіль марки ${this.model} має ${this.headlights} та ${this.wheels}, коштує ${this.price}`
+//     }
+// }
+// const info1 = {
+//     wheels: 4,
+//     headlights: 2,
+//     model: 'BMW M4',
+//     price: 150000
+// }
+// const bmw = new Car(info1)
+// console.log(bmw.showInfo())
+
+// const info2 = {
+//     wheels: 4,
+//     headlights: 2,
+//     model: 'Lexus RX',
+//     price: 200000
+// }
+// const lexus = new Car(info2)
+// console.log(lexus.showInfo())
+
+// Напишемо гру
+
+class Ork {
+    constructor({orkName, orkPoints, orkGender, orkSkinColor, orkWeapons}){
+        this.name = orkName;
+        this.points = orkPoints;
+        this.gender = orkGender;
+        this.skinColor = orkSkinColor;
+        this.weapons = orkWeapons
+    }
+    showOrkPoints(){
+        return `Ork ${this.name} has ${this.points}`
+    }
+    orkAttacks(){
+        this.points += 100
+        return `Ork ${this.name} attacks Elfs with ${this.weapons} and has ${this.points}`
+    }
+}
+const orkRufus = new Ork({
+    orkName: 'Rufus',
+    orkPoints: 100,
+    orkGender: 'male',
+    orkSkinColor: 'green',
+    orkWeapons: 'gun'
+})
+console.log(orkRufus.showOrkPoints())
+console.log(orkRufus.orkAttacks())
