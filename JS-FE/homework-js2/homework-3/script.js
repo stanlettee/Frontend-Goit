0b424bc4830eccb892c7037adf68e61e496b28ff
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-03-23 14:09:14 +0000

    homework 3

---
+++ JS-FE/homework-js2/homework-3/script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById('loadImages')
+button.addEventListener('click', () => {
+    const images = document.querySelectorAll('img[data-src]');
+    const options = {
+        root: null,
+        threshold: 0.1
+    };
+
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.getAttribute('data-src');
+                img.removeAttribute('data-src');
+                img.classList.add('loaded');
+                observer.unobserve(img);
+            }
+        });
+    }, options);
+
+    images.forEach(img => imageObserver.observe(img));
+});
+
